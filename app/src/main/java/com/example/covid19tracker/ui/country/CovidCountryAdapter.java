d67961d61e299671aaf91723df4c2503f103a4e7
DeanC <dean.clancy91@gmail.com>
2020-03-28 15:24:16 -0700

    Adapter complete

--- app/src/main/java/com/example/covid19tracker/ui/country/CovidCountryAdapter.java
+++ app/src/main/java/com/example/covid19tracker/ui/country/CovidCountryAdapter.java
@@ -1,33 +1,54 @@
 package com.example.covid19tracker.ui.country;
 
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.covid19tracker.R;
+
+import java.util.ArrayList;
+
 public class CovidCountryAdapter extends RecyclerView.Adapter<CovidCountryAdapter.ViewHolder> {
 
+    // ArrayList to hold the countries
+    ArrayList<CovidCountry> covidCountries;
+
+    public CovidCountryAdapter(ArrayList<CovidCountry> covidCountries){
+        this.covidCountries = covidCountries;
+    }
 
     @NonNull
     @Override
     public CovidCountryAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_list_covid_country, parent, false);
+        return new ViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull CovidCountryAdapter.ViewHolder holder, int position) {
-
+        CovidCountry covidCountry = covidCountries.get(position);
+        holder.tvTotalCases.setText(covidCountry.getmCases());
+        holder.tvCountryName.setText((covidCountry.getmCovidCountry()));
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return covidCountries.size();
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
+
+        TextView tvTotalCases, tvCountryName;
+
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
-        }
+            tvTotalCases = itemView.findViewById(R.id.tvTotalCases);
+            tvCountryName = itemView.findViewById(R.id.tvCountryName);
     }
+}
+
 }
