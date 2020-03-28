6997b57d189dbab17096f9126f55674a302295e5
DeanC <dean.clancy91@gmail.com>
2020-03-27 20:23:53 -0700

    Added getData method to HomeFragment.java

--- app/src/main/java/com/example/covid19tracker/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/covid19tracker/ui/home/HomeFragment.java
@@ -1,6 +1,7 @@
 package com.example.covid19tracker.ui.home;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,7 +10,19 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.covid19tracker.R;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.lang.ref.ReferenceQueue;
 
 public class HomeFragment extends Fragment {
 
@@ -33,5 +46,32 @@ public class HomeFragment extends Fragment {
     }
 
     private void getData() {
+        RequestQueue queue = Volley.newRequestQueue(getActivity());
+        String url = "https://corona.lmao.ninja/all";
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                progressBar.setVisibility(View.GONE);
+
+                try {
+                    JSONObject jsonObject = new JSONObject(response.toString());
+                    tvTotalConfirmed.setText(jsonObject.getString("cases"));
+                    tvTotalDeaths.setText(jsonObject.getString("deaths"));
+                    tvTotalRecovered.setText(jsonObject.getString("recovered"));
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                progressBar.setVisibility(View.GONE);
+                Log.d("Error Response", error.toString());
+            }
+    });
+
+        queue.add(stringRequest);
     }
 }
